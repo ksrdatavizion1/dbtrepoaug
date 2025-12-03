3d2b8f83d493ac30ada423cc47750f03444aea30
hemof65874@besenica.com <hemof65874@besenica.com>
2025-12-03 02:47:31 +0000

    new changes

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,5 +1,13 @@
 
 -- Use the `ref` function to select from other models
+{{
+    config(
+        materialized='table',
+        query_tag = 'DBT',
+        transient = false
+    )
+}}
+
 
 select *
 from {{ ref('my_first_dbt_model') }}
